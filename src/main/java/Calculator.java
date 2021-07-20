fca8f2392e507dae73322593a0b59613849888a3
Natanahalli Vishwanath <manoj.natanahalli-vishwanath@capgemini.com>
2021-07-20 12:45:59 +0530

    my second commit

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -3,10 +3,10 @@ public class Calculator {
 
 	public static void main(String[] args) {
 		
-		int a = 10;
-		int b = 20;
+		int aa = 10;
+		int bb = 20;
 		
-		System.out.println(a+b);
+		System.out.println(aa+bb);
 	}
 
 }
